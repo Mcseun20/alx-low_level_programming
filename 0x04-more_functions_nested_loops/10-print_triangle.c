f7b89f80a84aeda1642749e48bfa087aaba7d899
Mcseun20 <okeleyemusbau@gmail.com>
2023-08-03 10:46:37 +0100

    a function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ *
+ * Inspired by ALX
+ * October, 21-2022
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+
+int a, b;
+
+for (a = 1; a <= size; a++)
+{
+for (b = a; b < size; b++)
+{
+_putchar(' ');
+}
+
+for (b = 1; b <= a; b++)
+{
+_putchar('#');
+}
+
+_putchar('\n');
+}
+}
+}
